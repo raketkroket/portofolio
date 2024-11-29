d357cf78dcb0ce7388c10589ad10af4a25dcf987
raketkroket <vanderbergannemike@gmail.com>
2024-11-29 17:37:29 +0100

    Delete java.js

--- java.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-    document.querySelectorAll('.content').forEach(s => {
-        if (s.getBoundingClientRect().top < window.innerHeight - 100) s.classList.add('show');
-    });
-});
